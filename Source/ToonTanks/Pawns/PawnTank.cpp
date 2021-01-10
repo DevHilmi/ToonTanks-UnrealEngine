6e978b5dbd44e11cf6a466638dd9064c981b7775
Hilmi Ilyas Rizaldi <developer.hilmi@gmail.com>
2021-01-11 00:06:16 +0700

    Add simple movement code
    Move and Rotate

--- Source/ToonTanks/Pawns/PawnTank.cpp
+++ Source/ToonTanks/Pawns/PawnTank.cpp
@@ -1,6 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "PawnTank.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
@@ -11,14 +10,13 @@ APawnTank::APawnTank()
 	SpringArm->SetupAttachment(RootComponent);
 
 	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
-    SpringArm->SetupAttachment(SpringArm);
+	Camera->SetupAttachment(SpringArm);
 }
 
 // Called when the game starts or when spawned
 void APawnTank::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
@@ -26,11 +24,36 @@ void APawnTank::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	Rotate();
+	Move();
 }
 
 // Called to bind functionality to input
-void APawnTank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+void APawnTank::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAxis("MoveForward", this, &APawnTank::CalculateMoveInput);
+	PlayerInputComponent->BindAxis("Turn", this, &APawnTank::CalculateRotateInput);
+}
 
+void APawnTank::CalculateMoveInput(float Value)
+{
+	MoveDirection = FVector(Value * MoveSpeed * GetWorld()->DeltaTimeSeconds, 0, 0);
+}
+
+void APawnTank::CalculateRotateInput(float Value)
+{
+	float RotateAmount = Value * RotateSpeed * GetWorld()->DeltaTimeSeconds;
+	FRotator Rotation = FRotator(0, RotateAmount, 0);
+	RotationDirection = FQuat(Rotation);
+}
+
+void APawnTank::Move()
+{
+	AddActorLocalOffset(MoveDirection, true);
+}
+
+void APawnTank::Rotate()
+{
+	AddActorLocalRotation(RotationDirection, true);
 }
